9c0ad5d5e34e155186c6c11b387c5773fb2cc886
Taylor Ng <taylormng95@gmail.com>
2024-06-13 09:14:41 -0400

    creates NavBar

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom"
+import NavBar from "../components/NavBar"
 
 function App() {
 
   return (
     <>
+      <header>
+        <NavBar />
+      </header>
       <h1>App Page</h1>
       <Outlet />
     </>
