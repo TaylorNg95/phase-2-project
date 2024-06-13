a91a1bdfe2e82c18209341ddaf44b649f655f5c1
Taylor Ng <taylormng95@gmail.com>
2024-06-13 13:38:26 -0400

    completes search feature

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,11 +1,15 @@
-function FilterBar({filter, setFilter}) {
+function FilterBar({filter, setFilter, search, setSearch}) {
     return (
         <>
             <label>Filter Class:
                 <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                    <option value='All'>All</option>
                     <option value='2025'>2025</option>
                     <option value='2026'>2026</option>
                 </select>
+            </label>
+            <label>Search by Name:
+                <input type='text' value={search} onChange={(e) => setSearch(e.target.value)}/>
             </label>
         </>
     )
--- src/pages/PlayerList.jsx
+++ src/pages/PlayerList.jsx
@@ -5,16 +5,21 @@ import PlayerCard from "../components/PlayerCard"
 
 function PlayerList() {
     const {players} = useOutletContext()
-    const [filter, setFilter] = useState('2025')
-    console.log(players[1].classYear)
+    const [filter, setFilter] = useState('All')
+    const [search, setSearch] = useState('')
 
     const displayedPlayers = players.sort((a, b) => a.fname.toLowerCase().localeCompare(b.fname.toLowerCase()))
-        .filter(player => player.classYear == filter)
+        .filter(player => {
+            if(filter === 'All'){
+                return player
+            } else return player.classYear == filter
+        })
+        .filter(player => (player.fname + ' ' + player.lname).toLowerCase().includes(search.toLowerCase()))
 
     return (
         <>
             <h1>Player List Page</h1>
-            <FilterBar filter={filter} setFilter={setFilter}/>
+            <FilterBar filter={filter} setFilter={setFilter} search={search} setSearch={setSearch}/>
             {displayedPlayers.map(player => <PlayerCard key={player.id} player={player}/>)}
         </>
     )
