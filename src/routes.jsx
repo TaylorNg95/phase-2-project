942d44495bb76e30cd3511b18ece4b0ec9b9b701
Taylor Ng <taylormng95@gmail.com>
2024-06-13 09:15:22 -0400

    adds ErrorPage component

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,13 @@ import App from "./pages/App"
 import PlayerList from "./pages/PlayerList"
 import NewPlayerForm from "./pages/NewPlayerForm"
 import Notes from "./pages/Notes"
-/* import ErrorPage from "./pages/ErrorPage" */
+import ErrorPage from "./pages/ErrorPage"
 
 const routes = [
     {
         path: '/',
         element: <App />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/players',
