f5c39667b2d1eedc22dffacceab5e122956d9b0c
Taylor Ng <taylormng95@gmail.com>
2024-06-17 09:23:40 -0400

    styles nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,68 @@
 import { NavLink } from 'react-router-dom';
+import React from 'react';
+
+// MUI
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+
+/* aria-hidden=true */
 
 export default function NavBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <nav>
-      <NavLink to='/players'>
-        View All Players
-      </NavLink>
-      <NavLink to='/new'>
-        Add New Player
-      </NavLink>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenu}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem component={NavLink} onClick={handleClose} to='/players'><VisibilityOutlinedIcon aria-hidden='true'/>View All Players</MenuItem>
+            <MenuItem component={NavLink} onClick={handleClose} to='/new'><AddCircleOutlineOutlinedIcon aria-hiddne='true'/>Add New Entry</MenuItem>
+          </Menu>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Recruiting Database
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
